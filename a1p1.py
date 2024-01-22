ca56f41444fe5e3d4e8fc6994a80bec6dc12f49e
Kelly <kbach3@uci.edu>
2024-01-22 14:01:38 -0800

    Coded option r

--- a1p1.py
+++ a1p1.py
@@ -10,8 +10,8 @@ searches = search.split(' ')
 command = searches[0]
 directory = Path(searches[1])
 
-def get_files():
-    for document in directory.iterdir():
+def get_files(dir):
+    for document in dir.iterdir():
         if document.is_file():
             print(document)
 
@@ -19,13 +19,37 @@ def get_dir():
     for document in directory.iterdir():
         if document.is_dir():
             print(document)
+            return document
  
 def read_dir():
     get_files()
     get_dir()
 
+def option_r():
+    get_files(directory)
+    for item in directory.iterdir():
+        if item.is_dir():
+            print(item)
+            get_files(item)
+
+def option_s():
+    pass
+def option_e():
+    pass
+
 if __name__ == '__main__':
     if command == 'L':
-        read_dir()
+        if len(searches) == 3:
+            option = searches[2]
+            if option == '-r':
+                option_r()
+            elif option == 'f':
+                get_files()
+            elif option == '-s':
+                pass
+            elif option == 'e':
+                pass
+        else: 
+            read_dir()
     elif command == 'Q':
         exit()
